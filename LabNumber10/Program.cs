36b07e50e7f3dfde8f7811fc5f2f36043efc6fe9
Aleena Tucker <lena.tucker@outlook.com>
2016-11-08 09:20:51 -0500

    cleaned up the movie list region a bit, removed redundancy

--- LabNumber10/Program.cs
+++ LabNumber10/Program.cs
@@ -17,27 +17,18 @@ namespace LabNumber10
             ;
             string category, cont = "y";
 #region  // Movie List 
-            Movie Movie1 = new Movie("A Bug's Life", "animated", "1");
-            movies.Add(Movie1);
-            Movie Movie2 = new Movie("Meet the Robinsons", "animated", "1");
-            movies.Add(Movie2);
-            Movie Movie3 = new Movie("Grandma's Boy", "comedy", "2");
-            movies.Add(Movie3);
-            Movie Movie4 = new Movie("Mad Max", "action", "3");
-            movies.Add(Movie4);
-            Movie Movie5 = new Movie("Young Frankenstein", "comedy", "2");
-            movies.Add(Movie5);
-            Movie Movie6 = new Movie("Iron Man", "action", "3");
-            movies.Add(Movie6);
-            Movie Movie7 = new Movie("James and the Giant Peach", "animated", "1");
-            movies.Add(Movie7);
-            Movie Movie8 = new Movie("Ace Ventura: Pet Detective", "comedy", "2");
-            movies.Add(Movie8);
-            Movie Movie9 = new Movie("Con Air", "action", "3");
-            movies.Add(Movie9);
-            Movie Movie10 = new Movie("Wreck-It Ralph", "animated", "1");
-            movies.Add(Movie10);
+            movies.Add(new Movie("A Bug's Life", "animated", "1"));
+            movies.Add(new Movie("Meet the Robinsons", "animated", "1"));
+            movies.Add(new Movie("Grandma's Boy", "comedy", "2"));
+            movies.Add(new Movie("Mad Max", "action", "3"));
+            movies.Add(new Movie("Young Frankenstein", "comedy", "2"));
+            movies.Add(new Movie("Iron Man", "action", "3"));
+            movies.Add(new Movie("James and the Giant Peach", "animated", "1"));
+            movies.Add(new Movie("Ace Ventura: Pet Detective", "comedy", "2"));
+            movies.Add(new Movie("Con Air", "action", "3"));
+            movies.Add(new Movie("Wreck-It Ralph", "animated", "1"));
             #endregion
+
             Console.WriteLine("Welcome to the Movie List Application!");
             Console.WriteLine();
             Console.WriteLine("There are 100 movies in this list from the following categories: \n 1. Animated, \n 2. Comedy, \n 3. Action.");
